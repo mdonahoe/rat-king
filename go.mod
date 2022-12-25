44dd20c874f3d0a849553534a9cf3ba16d43b84b
Matt Donahoe <matt.donahoe@gmail.com>
2022-12-25 07:01:35 +0000

    execute commands from a file

--- go.mod
+++ go.mod
@@ -2,10 +2,13 @@ module github.com/mdonahoe/rat-king
 
 go 1.19
 
-require github.com/briandowns/spinner v1.20.0
+require (
+	github.com/briandowns/spinner v1.20.0
+	github.com/fatih/color v1.7.0
+	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510
+)
 
 require (
-	github.com/fatih/color v1.7.0 // indirect
 	github.com/mattn/go-colorable v0.1.2 // indirect
 	github.com/mattn/go-isatty v0.0.8 // indirect
 	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
--- main.go
+++ main.go
@@ -1,35 +1,77 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/briandowns/spinner"
+	"github.com/fatih/color"
+	"github.com/google/shlex"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
 	"time"
 )
 
+// hold the command strings
+type Commands []string
+
+func parseCommandsFromFile(f *os.File) Commands {
+	// Prep an array of command strings
+	cmds := make(Commands, 0, 10)
+
+	// Wrapping the unbuffered `*os.File` with a buffered reader gives us
+	// the `ReadString` method which will let us read entire lines.
+	rdr := bufio.NewReader(f)
+
+	// `ReadString` returns the next string from the
+	// input up to the given separator byte.
+	for {
+		// Switch on the err after reading a single line from the input file.
+		switch line, err := rdr.ReadString('\n'); err {
+
+		// The err is nil, so the read succeeded.
+		case nil:
+			cmds = append(cmds, line)
+
+		// End of input.
+		case io.EOF:
+			return cmds
+
+		// There was a problem!
+		default:
+			fmt.Fprintln(os.Stderr, "error:", err)
+			os.Exit(1)
+		}
+	}
+}
+
 func main() {
+	cmds := parseCommandsFromFile(os.Stdin)
 	s := spinner.New(spinner.CharSets[14], 100*time.Millisecond) // Build our new spinner
-	s.Start()                                                    // Start the spinner
-	s.Prefix = "Lexing "
-	s.Color("green", "bold")
-	time.Sleep(1 * time.Second) // Run for some time to simulate work
-	s.Stop()
-	fmt.Println("\rLexed.")
-	s.Color("cyan")
-	s.Prefix = "Compiling "
-	s.Start()
-	time.Sleep(1 * time.Second) // Run for some time to simulate work
-	s.Stop()
-	fmt.Println("Compiled.")
-	s.Prefix = "Linking "
-	s.Color("red", "bold")
-	s.Start()
-	time.Sleep(1 * time.Second) // Run for some time to simulate work
-	s.Stop()
-	fmt.Println("Linked.")
-	s.Prefix = "Installing "
-	s.Color("magenta", "bold")
-	s.Start()
-	time.Sleep(1 * time.Second) // Run for some time to simulate work
-	s.Stop()
-	fmt.Println("Installed!")
+	for _, line := range cmds {
+		trimmedLine := strings.TrimRight(line, "\n")
+		s.Suffix = " " + trimmedLine
+		s.Color("green", "bold")
+		s.Start()
+
+		// parse and run the cmd, printing output
+		cmdline, err3 := shlex.Split(line)
+		if err3 != nil {
+			log.Fatal(err3)
+		}
+		cmd := exec.Command(cmdline[0], cmdline[1:]...)
+		out, err := cmd.Output()
+		s.Stop()
+		if err != nil {
+			d := color.New(color.FgRed, color.Bold)
+			d.Printf("%s\n", trimmedLine)
+			fmt.Printf("%s\n\n", err)
+		} else {
+			d := color.New(color.FgGreen, color.Bold)
+			d.Printf("%s\n", trimmedLine)
+			fmt.Printf("%s\n", out)
+		}
+	}
 }
